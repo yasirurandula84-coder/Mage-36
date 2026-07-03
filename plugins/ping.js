a9e5acc7078e082d22e0f15af3792893059d69fd
yasirurandula84-coder <yasirurandula84@gmail.com>
2026-07-03 16:31:34 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -8,10 +8,19 @@ cmd(
     category: "main",
     filename: __filename,
   },
-  async (danuwa, mek, m, { reply }) => {
+  async (danuwa, mek, m, { from, reply }) => {
     const start = Date.now();
-    await reply("🏓 Pinging...");
+    
+    // මුලින්ම පණිවිඩය යවලා ඒකේ key එක ගන්නවා
+    const sentMsg = await reply("🏓 Pinging...");
+    
     const end = Date.now();
-    await reply(`🏓 Pong! Response time: *${end - start}ms*`);
+    const speed = end - start;
+
+    // දැන් අර යවපු message එක Edit කරනවා
+    await danuwa.sendMessage(from, { 
+        text: `🏓 Pong! \n\n*Response speed:* ${speed}ms`,
+        edit: sentMsg.key 
+    });
   }
 );
